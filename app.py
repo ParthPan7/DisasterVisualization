618150f7d1ad2aef1bbe0ab0818334dcf4f9e708
Parth Panchal <pr930523@dal.ca>
2020-03-03 12:17:49 -0400

    "Intial Project Structure"

---
+++ app.py
@@ -0,0 +1,15 @@
+import json
+
+import flask
+import pandas as pd
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def index():
+
+    return flask.render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
