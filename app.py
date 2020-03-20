bf8ccd5712f82e6b006c63707d6b368cd1baaf74
Parth Panchal <pr930523@dal.ca>
2020-03-20 15:15:16 -0300

    "+ Dashboard added" "+ interactive Choropleth added" "+ donut chart added " "+ interactive line graph added"

--- app.py
+++ app.py
@@ -10,7 +10,16 @@ def index():
 
     return flask.render_template("index.html")
 
-
+@app.route("/map")
+def map():
+    sourceDataFrame = pd.read_csv("disaster.csv")
+    dataFrameCountryMapping = pd.read_csv("countryMapping.csv")
+    dataFrameCountryMapping = dataFrameCountryMapping.to_dict(orient='record')
+    dataFrameCountryMapping = json.dumps(dataFrameCountryMapping)
+    disasterData = sourceDataFrame.to_dict(orient='records')
+    disasterData =  json.dumps(disasterData,indent=2)
+    data = {'plot_data':disasterData,'country_mapping_data':dataFrameCountryMapping}
+    return flask.render_template("worldmap.html",data = data)
 @app.route("/requestRadialStackBar")
 def radialStackBar():
     stackBarData = pd.read_csv("stackBarData.csv")
