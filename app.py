1594ea7758d489443e634313aae4f2be04634336
Parth Panchal <pr930523@dal.ca>
2020-03-22 12:52:28 -0300

    "+Improving interactivity in Line trend"

--- app.py
+++ app.py
@@ -4,13 +4,8 @@ import flask
 import pandas as pd
 
 app = flask.Flask(__name__)
-
-@app.route("/")
-def index():
-
-    return flask.render_template("index.html")
-
 @app.route("/map")
+@app.route("/")
 def map():
     sourceDataFrame = pd.read_csv("disaster.csv")
     dataFrameCountryMapping = pd.read_csv("countryMapping.csv")
@@ -19,7 +14,7 @@ def map():
     disasterData = sourceDataFrame.to_dict(orient='records')
     disasterData =  json.dumps(disasterData,indent=2)
     data = {'plot_data':disasterData,'country_mapping_data':dataFrameCountryMapping}
-    return flask.render_template("worldmap.html",data = data)
+    return flask.render_template("worldmap.html", data=data)
 @app.route("/requestRadialStackBar")
 def radialStackBar():
     stackBarData = pd.read_csv("stackBarData.csv")
@@ -29,7 +24,7 @@ def radialStackBar():
     dataFrameCountryMapping = dataFrameCountryMapping.to_dict(orient='record')
     dataFrameCountryMapping = json.dumps(dataFrameCountryMapping)
     data = {'stack_bar_data':stackBarData,'country_mapping_data':dataFrameCountryMapping}
-    return flask.render_template("radialStackBar.html",data=data)
+    return flask.render_template("radialStackBar.html", data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
